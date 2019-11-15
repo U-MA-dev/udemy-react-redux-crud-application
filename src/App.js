0272d2c52010fa9a45a76430a2e3b49d703de2bc
U-MA-dev <yasunarist.UMA.dev@gmail.com>
2019-11-16 00:07:49 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <h1>hello, world!</h1>
-  );
+class App extends Component {
+  render(){
+    const dom = (
+      <>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked!")}} />
+      </>
+    )
+    return dom;
+  }
 }
 
 export default App;
